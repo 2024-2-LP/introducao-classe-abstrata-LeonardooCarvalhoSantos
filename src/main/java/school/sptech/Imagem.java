a689f206d2a65daddf16e6b5b972e1447fe2230d
LeonardooCarvalhoSantos <leonardo.csantos@sptech.school>
2024-11-07 18:10:54 -0300

    Leonardo de Carvalho Santos - Atividade Concluida

--- src/main/java/school/sptech/Imagem.java
+++ src/main/java/school/sptech/Imagem.java
@@ -1,4 +1,54 @@
 package school.sptech;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Imagem {
+    private List<Figura> figuras = new ArrayList<>();
+
+    public List<Figura> getFiguras() {
+        return figuras;
+    }
+
+    public void setFiguras(List<Figura> figuras) {
+        this.figuras = figuras;
+    }
+
+    public void adicionar(Figura figura) {
+        figuras.add(figura);
+    }
+
+    public Double calcularSomaDasAreas() {
+        Double somaAreas = 0.0;
+
+        for (Figura figura : figuras) {
+            somaAreas += figura.calcularArea();
+        }
+
+        return somaAreas;
+    }
+
+    public List<Figura> buscarPorAreaMaiorQue20() {
+        List<Figura> figurasMaior20 = new ArrayList<>();
+
+        for (Figura figura : figuras) {
+            if (figura.calcularArea() > 20) {
+                figurasMaior20.add(figura);
+            }
+        }
+
+        return figurasMaior20;
+    }
+
+    public List<Figura> buscarQuadrados() {
+        List<Figura> figurasQuadradas = new ArrayList<>();
+
+        for (Figura figura : figuras) {
+            if (figura instanceof Quadrado) {
+                figurasQuadradas.add(figura);
+            }
+        }
+
+        return figurasQuadradas;
+    }
 }
